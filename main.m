6314e80daa6e8e7f19d7d626bfa99e2199a46f2e
Saif Mamat <smamat@gmail.com>
2012-05-16 11:03:31 +0800

    first commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  jTile
+//
+//  Created by Saif Mamat on 11/05/2012.
+//  Copyright 2012 DD Multimedia Solutions Sdn. Bhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
